399a027b4fcf9438597a8f8edaad17bb482ee8b4
Gisela Acosta <gacosta@technisys.com>
2020-09-10 22:55:01 -0300

    c

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -8,44 +8,45 @@ import {TimerObservable} from "rxjs/observable/TimerObservable";
   templateUrl: './agilidad-aritmetica.component.html',
   styleUrls: ['./agilidad-aritmetica.component.css']
 })
-export class AgilidadAritmeticaComponent implements OnInit {
-   @Output() 
-  enviarJuego :EventEmitter<any>= new EventEmitter<any>();
-  nuevoJuego : JuegoAgilidad;
-  ocultarVerificar: boolean;
-  Tiempo: number;
-  repetidor:any;
-  private subscription: Subscription;
-  ngOnInit() {
-  }
-   constructor() {
-     this.ocultarVerificar=true;
-     this.Tiempo=5; 
-    this.nuevoJuego = new JuegoAgilidad();
-    console.info("Inicio agilidad");  
-  }
-  NuevoJuego() {
-    this.ocultarVerificar=false;
-   this.repetidor = setInterval(()=>{ 
+export class AgilidadAritmeticaComponent {
+// implements OnInit {
+//    @Output() 
+//   enviarJuego :EventEmitter<any>= new EventEmitter<any>();
+//   nuevoJuego : JuegoAgilidad;
+//   ocultarVerificar: boolean;
+//   Tiempo: number;
+//   repetidor:any;
+//   private subscription: Subscription;
+//   ngOnInit() {
+//   }
+//    constructor() {
+//      this.ocultarVerificar=true;
+//      this.Tiempo=5; 
+//     this.nuevoJuego = new JuegoAgilidad();
+//     console.info("Inicio agilidad");  
+//   }
+//   NuevoJuego() {
+//     this.ocultarVerificar=false;
+//    this.repetidor = setInterval(()=>{ 
       
-      this.Tiempo--;
-      console.log("llego", this.Tiempo);
-      if(this.Tiempo==0 ) {
-        clearInterval(this.repetidor);
-        this.verificar();
-        this.ocultarVerificar=true;
-        this.Tiempo=5;
-      }
-      }, 900);
+//       this.Tiempo--;
+//       console.log("llego", this.Tiempo);
+//       if(this.Tiempo==0 ) {
+//         clearInterval(this.repetidor);
+//         this.verificar();
+//         this.ocultarVerificar=true;
+//         this.Tiempo=5;
+//       }
+//       }, 900);
 
-  }
-  verificar()
-  {
-    this.ocultarVerificar=false;
-    clearInterval(this.repetidor);
+//   }
+//   verificar()
+//   {
+//     this.ocultarVerificar=false;
+//     clearInterval(this.repetidor);
    
 
    
-  }  
+//   }  
 
 }
--- src/app/componentes/mapa-de-google/mapa-de-google.component.spec.ts
+++ src/app/componentes/mapa-de-google/mapa-de-google.component.spec.ts
@@ -1,25 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapaDeGoogleComponent } from './mapa-de-google.component';
+//import { MapaDeGoogleComponent } from './mapa-de-google.component';
 
-describe('MapaDeGoogleComponent', () => {
-  let component: MapaDeGoogleComponent;
-  let fixture: ComponentFixture<MapaDeGoogleComponent>;
+// describe('MapaDeGoogleComponent', () => {
+//   let component: MapaDeGoogleComponent;
+//   let fixture: ComponentFixture<MapaDeGoogleComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MapaDeGoogleComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ MapaDeGoogleComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MapaDeGoogleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MapaDeGoogleComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
