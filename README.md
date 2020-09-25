6dfc9ad3bd6659444a25c881e880521a81e0dd34
Gisela Acosta <gacosta@technisys.com>
2020-09-25 00:57:03 -0300

    cambio nombre app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Juegos
+# sala-de-juegos
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('juegos app is running!');
+    expect(page.getTitleText()).toEqual('sala-de-juegos app is running!');
   });
 
   afterEach(async () => {
